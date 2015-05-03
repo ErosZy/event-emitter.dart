4ede9ca9d28c7fbbbe5604d18d8d264bf57bb693
Jakub Turek <ja@kubaturek.pl>
2015-05-03 12:00:36 +0200

    Add event objects' tests to travis builds

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,8 +1,10 @@
 library test.event_emitter;
 
 import 'src/event_emitter_test.dart' as event_emitter_test;
+import 'src/event_emitter_with_event_objects_test.dart' as event_objects_test;
 import 'package:test/test.dart';
 
 void main () {
     group('[Event emitter]', event_emitter_test.main);
+    group('[Event emitter with objects]', event_objects_test.main);
 }
