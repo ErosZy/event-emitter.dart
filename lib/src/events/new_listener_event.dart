064c998bd9a433179fcf0b25c4a144058421a84f
Jakub Turek <ja@kubaturek.pl>
2015-05-04 19:19:47 +0200

    Create NewListener and RemoveListener event classes

---
+++ lib/src/events/new_listener_event.dart
@@ -0,0 +1,7 @@
+library event_emitter.event.new_listener;
+
+import './../event_interface.dart';
+
+class NewListenerEvent implements EventInterface {
+    static const NAME = 'newListener';
+}
---
+++ lib/src/events/remove_listener_event.dart
@@ -0,0 +1,7 @@
+library event_emitter.event.remove_listener;
+
+import './../event_interface.dart';
+
+class RemoveListenerEvent implements EventInterface {
+    static const NAME = 'removeListener';
+}
